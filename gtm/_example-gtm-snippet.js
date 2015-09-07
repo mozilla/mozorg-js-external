201063deb155d889e0cb34ff65d572bda7817454
Alex Gibson <agibson@mozilla.com>
2015-09-07 11:37:01 +0100

    Add example snippets and update documentation

---
+++ gtm/_example-gtm-snippet.js
@@ -0,0 +1,9 @@
+/**
+ * Description: (e.g. Home page promo link clicks.)
+ * Bug:
+ * Target URL: (e.g. https://www.mozilla.org/)
+ */
+
+// ... firing conditions ...
+
+// ... experiment code here ...
---
+++ optimizely/0123456789-example.js
@@ -0,0 +1,23 @@
+/**
+ * Experiment ID: xxxxxxxxxx
+ * Description: (e.g. Windows 10 Edge Survey)
+ * Mana URL:
+ * Bug URL:
+ * Target URL: (e.g. https://www.mozilla.org/en-US/firefox/new/)
+ * Locale: (e.g en-US)
+ * Duration: (e.g. July 30 - August 24 2015)
+ */
+
+// Regexp used for targeting
+function snippetRegexp() {
+    'use strict';
+    return; // regexp goes here.
+}
+
+// Variation A
+function variationA() {
+    'use strict';
+    // snippet variation A JS goes here.
+}
+
+// Variation B and so on...
